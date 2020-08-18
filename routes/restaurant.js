b6aafac3979ac47631f3d3206c1e1add3a3789b1
GGG <ggg@MacBook-Pro-de-GGG.local>
2020-08-18 16:33:07 +0200

    resto/:id

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -4,8 +4,14 @@ const liste = require("../assets/restaurants.json");
 // console.log(liste);
 
 router.get("/liste/restaurants", (req, res) => {
-  const restaurant = liste;
-  res.json(restaurant);
-  console.log(restaurant);
+  const restaurants = liste;
+  res.json(restaurants);
+  console.log(restaurants);
 });
+
+router.get("liste/restaurant/:id"), (req, res)=>{
+const restaurant = await liste.findById({id: req.query.id });
+console.log(restaurant);
+res.json(restaurant);
+}
 module.exports = router;
