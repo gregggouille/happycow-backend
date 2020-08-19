76223d50ca5bf3e32157c4d1d8e8b7f5c83f52c0
GGG <ggg@MacBook-Pro-de-GGG.local>
2020-08-19 09:56:35 +0200

    add async

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const liste = require("../assets/restaurants.json");
 // console.log(liste);
 
-router.get("/liste/restaurants", (req, res) => {
-  const restaurants = liste.find();
+router.get("/liste/restaurants", async (req, res) => {
+  const restaurants = await liste.find();
   res.json(restaurants);
   console.log(restaurants);
 });
 
-router.get("liste/restaurant/:id"), (req, res)=>{
-const restaurant = await liste.findById({id: req.query.id });
-console.log(restaurant);
-res.json(restaurant);
-}
+router.get("liste/restaurant/:id"),
+  async (req, res) => {
+    const restaurant = await liste.findById({ id: req.query.id });
+    console.log(restaurant);
+    res.json(restaurant);
+  };
 module.exports = router;
